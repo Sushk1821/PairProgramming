b39221b250bdc5923f52b829c52cdb24e24d6210
Pratik Nalage <pratiknalage@Pratiks-MacBook.local>
2022-02-24 22:48:43 -0500

    comments

--- Roman.py
+++ Roman.py
@@ -1,5 +1,8 @@
-
+# Convert roman to integer
 def romanToInt(self, s: str) -> int:
+    """
+    Map a roman_int list. 
+    """
     roman_to_int = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
     roman = ["I","V","X","L","C","D","M"]
     int_num = 0
@@ -14,7 +17,11 @@ def romanToInt(self, s: str) -> int:
         print(int_num)
     return int_num
 
+# Convert integer to roman
 def intToRoman(self, num: int) -> str:
+    """
+    Map a int_roman list. 
+    """
     int_to_roman = {1000: 'M', 900: 'CM', 500: 'D', 400: 'CD', 100: 'C', 90: 'XC', 50: 'L', 40: 'XL', 10: 'X', 9: 'IX', 5: 'V', 4: 'IV', 1: 'I'}
     roman_num = ""
     while num > 0:
